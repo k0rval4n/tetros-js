c417cf72f20845c3c6f756493ba52ef09ab47a7f
k0rval4n <enrique.corvalan@uc.cl>
2025-04-12 16:27:04 -0400

    Implement Tetriminos

--- index.js
+++ index.js
@@ -23,6 +23,169 @@ class TetrisCanvasDrawer {
   }
 }
 
+class Tetrimino {
+  constructor(shapes, color) {
+    this.shapes = shapes;
+    this.actualShapeIndex = 0;
+    this.color = color;
+  }
+
+  get shape() {
+    return this.shapes[this.actualShapeIndex];
+  }
+
+  rotateToLeft() {
+    this.actualShapeIndex =
+      (this.actualShapeIndex - 1 + this.shapes.length) % this.shapes.length;
+  }
+
+  rotateToRight() {
+    this.actualShapeIndex = (this.actualShapeIndex + 1) % this.shapes.length;
+  }
+}
+
+class OTetrimino extends Tetrimino {
+  constructor() {
+    const shapes = [
+      [
+        [1, 1],
+        [1, 1],
+      ],
+    ];
+    super(shapes, "yellow");
+  }
+}
+
+class ITetrimino extends Tetrimino {
+  constructor() {
+    // prettier-ignore
+    const shapes = [
+      [
+        [1, 1, 1, 1],
+      ],
+      [
+        [1],
+        [1],
+        [1],
+        [1],
+      ],
+    ];
+    super(shapes, "cyan");
+  }
+}
+
+class JTetrimino extends Tetrimino {
+  constructor() {
+    const shapes = [
+      [
+        [0, 1],
+        [0, 1],
+        [1, 1],
+      ],
+      [
+        [1, 0, 0],
+        [1, 1, 1],
+      ],
+      [
+        [1, 1],
+        [1, 0],
+        [1, 0],
+      ],
+      [
+        [1, 1, 1],
+        [0, 0, 1],
+      ],
+    ];
+    super(shapes, "blue");
+  }
+}
+
+class LTetrimino extends Tetrimino {
+  constructor() {
+    const shapes = [
+      [
+        [1, 0],
+        [1, 0],
+        [1, 1],
+      ],
+      [
+        [1, 1, 1],
+        [1, 0, 0],
+      ],
+      [
+        [1, 1],
+        [0, 1],
+        [0, 1],
+      ],
+      [
+        [0, 0, 1],
+        [1, 1, 1],
+      ],
+    ];
+    super(shapes, "orange");
+  }
+}
+
+class STetrimino extends Tetrimino {
+  constructor() {
+    const shapes = [
+      [
+        [0, 1, 1],
+        [1, 1, 0],
+      ],
+      [
+        [1, 0],
+        [1, 1],
+        [0, 1],
+      ],
+    ];
+    super(shapes, "green");
+  }
+}
+
+class ZTetrimino extends Tetrimino {
+  constructor() {
+    const shapes = [
+      [
+        [1, 1, 0],
+        [0, 1, 1],
+      ],
+      [
+        [0, 1],
+        [1, 1],
+        [1, 0],
+      ],
+    ];
+    super(shapes, "red");
+  }
+}
+
+class TTetrimino extends Tetrimino {
+  constructor() {
+    const shapes = [
+      [
+        [1, 1, 1],
+        [0, 1, 0],
+      ],
+      [
+        [0, 1],
+        [1, 1],
+        [0, 1],
+      ],
+      [
+        [0, 1, 0],
+        [1, 1, 1],
+      ],
+      [
+        [1, 0],
+        [1, 1],
+        [1, 0],
+      ],
+    ];
+    super(shapes, "purple");
+  }
+}
+
 const backgroundCanvas = document.getElementById("background-canvas");
 const inGameCanvas = document.getElementById("in-game-canvas");
 const tetrisBackgroundDrawer = new TetrisCanvasDrawer(backgroundCanvas);
